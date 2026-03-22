681b5e0c0513c31b962fb23af1301f4ad75d6e60
Swanand Kavitkar <kavitkarswanand5@gmail.com>
2026-03-21 20:43:10 -0500

    Profile update

--- src/components/Resume/Work.jsx
+++ src/components/Resume/Work.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Work = (props) => {
   const { data } = props;
   return (
-    <div className="flex flex-col items-center justify-center lg:flex-row lg:gap-4">
+    <div className="flex flex-col items-center lg:flex-row lg:items-start lg:gap-4 w-full">
       <div className="flex flex-col items-center justify-center w-3/5 gap-2 m-2 text-center lg:w-2/5 lg:items-start">
         <div className="text-lg font-normal">{data.year}</div>
         <div className="text-lg font-bold">{data.organisation}</div>
